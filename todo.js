ad8446452fb6e80d924f3894959023a71f4127ef
Bui Khac Nam <buikhacnam11@gmail.com>
2020-09-10 15:03:25 +0700

    a

--- todo.js
+++ todo.js
@@ -6,6 +6,18 @@ const input = document.getElementById("input");
 
 
 
+//creating a list array:
+
+let listItems = [
+	{  title: "something",
+		trash: false,
+		done: false,
+		id: 0
+	}
+];
+
+
+
 //add an item to the list user the enter key
 input.addEventListener("keyup", newTodo);
 
@@ -16,12 +28,12 @@ input.addEventListener("keyup", newTodo);
 function newTodo(event) {
 	if(event.keyCode == 13) {
 		const position = "beforeend";
-		const what = input.value;
-		if (what == "") {
-			alert("todo cannot be blank");
+		const title = input.value;
+		if (title == "") {
+			input.blur();
 		} else {
 		item = ` <li class="item">
-                      <p class="text">${what}</p>
+                      <p class="text">${title}</p>
                      <p class="done"><i class="fas fa-check-circle" job="complete"></i></p>
                      <p class="erase"><i class="fas fa-trash-alt" job="delete"></i></p>  
                 </li>`;
@@ -36,18 +48,9 @@ function newTodo(event) {
 
 
 
-function completeTodo(element) {
-    //console.log(element.parentNode.parentNode.nodeName);
-    element.parentNode.parentNode.querySelector(".text").classList.toggle("text-complete");
-	console.log("complete");
-	element.parentNode.classList.toggle("after-done");;
-	element.parentNode.parentNode.querySelector(".erase").classList.toggle("after-erase");
-}
 
-function deleteTodo(element) {
-	console.log("delete");
-	element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
-}
+
+
 
 //test the trash and complete button:
 
@@ -56,13 +59,23 @@ list.addEventListener("click", function(event)  {
 	const deleteOrComplete = element.attributes.job.value;
 
 	if(deleteOrComplete == "complete"){
-		console.log("hey")
 		completeTodo(element);
 	} else if (deleteOrComplete == "delete"){
 		deleteTodo(element);
 	}
 })
 
+function completeTodo(element) {
+    console.log("complete");
+    element.parentNode.parentNode.querySelector(".text").classList.toggle("text-complete");
+	element.parentNode.classList.toggle("after-done");;
+	element.parentNode.parentNode.querySelector(".erase").classList.toggle("after-erase");
+}
+
+function deleteTodo(element) {
+	console.log("delete");
+	element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+}
 
 
 
@@ -81,7 +94,7 @@ function displayDate() {
 
 function dateBuilder(now) {
    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   
   let day = days[now.getDay()];
   let date = now.getDate();
