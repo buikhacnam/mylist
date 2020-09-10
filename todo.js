34d9bc57799828371c8c0242083dcb2532f17f9e
Bui Khac Nam <buikhacnam11@gmail.com>
2020-09-10 12:54:39 +0700

    123

--- todo.js
+++ todo.js
@@ -20,17 +20,48 @@ function newTodo(event) {
 		if (what == "") {
 			alert("todo cannot be blank");
 		} else {
-		item = `<li class="item">
-	                  <p class="text">${what}</p>
-	                  <p class="done"><i class="fas fa-check-circle"></i></p>
-	                 <p class=trash><i class="fas fa-trash-alt"></i></p>	
-        		</li>`;
+		item = ` <li class="item">
+                      <p class="text">${what}</p>
+                     <p class="done"><i class="fas fa-check-circle" job="complete"></i></p>
+                     <p class="erase"><i class="fas fa-trash-alt" job="delete"></i></p>  
+                </li>`;
 		list.insertAdjacentHTML(position, item);
 		input.value = "";
 		}	
 	}
 	
 }
+
+
+
+
+
+function completeTodo(element) {
+    //console.log(element.parentNode.parentNode.nodeName);
+    element.parentNode.parentNode.querySelector(".text").classList.toggle("text-complete");
+	console.log("complete");
+	element.parentNode.classList.toggle("after-done");;
+	element.parentNode.parentNode.querySelector(".erase").classList.toggle("after-erase");
+}
+
+function deleteTodo(element) {
+	console.log("delete");
+	element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+}
+
+//test the trash and complete button:
+
+list.addEventListener("click", function(event)  {
+	const element = event.target;
+	const deleteOrComplete = element.attributes.job.value;
+
+	if(deleteOrComplete == "complete"){
+		console.log("hey")
+		completeTodo(element);
+	} else if (deleteOrComplete == "delete"){
+		deleteTodo(element);
+	}
+})
 
 
 
